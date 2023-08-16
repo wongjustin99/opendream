af0726a180a31040f1c658ed104d5312ab728750
Justin Wong <1584142+wongjustin99@users.noreply.github.com>
2023-08-16 03:08:21 -0400

    run unicorn on all interfaces

--- run_opendream.sh
+++ run_opendream.sh
@@ -6,7 +6,7 @@ source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt 
 
-uvicorn opendream.server:app --reload &
+uvicorn opendream.server:app --host 0.0.0.0 --reload &
 
 cd webapp/opendream-ui && npm install && npm run start &
 
